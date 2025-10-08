aaa7b454c0a9c57430e9534d5bf3bc1e07fe9cfb
Marco Chierici <chierici@fbk.eu>
2025-10-08 13:40:09 +0200

    Added basic compute_stats function and usage

--- analysis.R
+++ analysis.R
@@ -1,4 +1,11 @@
 library(tidyverse)
 
+compute_stats <- function(x) {
+  c(m=mean(x))
+}
+
 data("mtcars")
 ggplot(mtcars, aes(x=disp, y=mpg)) + geom_point(aes(color=factor(cyl)))
+
+ret <- compute_stats(mtcars$mpg)
+print(ret)
